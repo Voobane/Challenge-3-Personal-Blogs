a4bfc373dd382a835fa81ce563c495aafc90a132
Matt <168368157+Voobane@users.noreply.github.com>
2024-06-23 17:46:07 -0400

    1single page done

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,1 +1,25 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const themeSwitcher = document.getElementById('themeToggle');
+    
+    // Check for saved user preference, if any, on load of the website
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+        if (currentTheme === 'dark-mode') {
+            themeSwitcher.checked = true;
+        }
+    } else {
+        document.body.classList.add('light-mode'); // Ensure light-mode by default
+    }
 
+    // Listen for the switcher to be clicked and then toggle the theme
+    themeSwitcher.addEventListener('change', function() {
+        if (this.checked) {
+            document.body.classList.replace('light-mode', 'dark-mode');
+            localStorage.setItem('theme', 'dark-mode');
+        } else {
+            document.body.classList.replace('dark-mode', 'light-mode');
+            localStorage.setItem('theme', 'light-mode');
+        }
+    });
+});
